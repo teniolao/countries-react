6765e343e21950dda52cf2c5cc8c2c5c61eab7a3
teniolao <Tennybetty100@gmail.com>
2022-03-28 21:45:22 +0100

    lesson 2 complete

--- src/App.js
+++ src/App.js
@@ -1,27 +1,51 @@
 import "./App.css";
-import country from "./countriesAll.json";
+import countries from "./countriesAll.json";
+import { useState } from "react";
+
 
 function formatNumber(num) {
   return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
 }
 const RenderCountries = (props) => {
-  return props.data.map((country, index) => {
+  return props.countries.map((country, index) => {
     return (
-      <div key={index}>
-        <img src={country.flag} alt={country.name} />
+      <div key={index} className="singleCountry">
+        <img alt={country.name} src={country.flag}></img>
         <h3>{country.name}</h3>
         <p>Population: {formatNumber(country.population)}</p>
         <p>Region: {country.region}</p>
-        <p>Capital: {country.capital}</p>
+        <p>Capital:{country.capital} </p>
       </div>
     );
   });
 };
 
 function App() {
+  const filterCountries = (typedValue) => {
+    return countries.filter((country) => {
+      return (
+        country.name.toLowerCase().includes(typedValue.toLowerCase()) ||
+        country.capital?.toLowerCase().includes(typedValue.toLowerCase())
+      );
+    });
+  };
+
+  const [arrayOfCountries, setArrayOfCountries] = useState(countries);
   return (
     <div className="App">
-      <RenderCountries data={country} />
+      <p>
+        Search:
+        <input
+          type="text"
+          onChange={(event) => {
+            setArrayOfCountries(filterCountries(event.target.value));
+          }}
+        ></input>
+      </p>
+
+      <div className="flex">
+        <RenderCountries countries={arrayOfCountries} />
+      </div>
     </div>
   );
 }
