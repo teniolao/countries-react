05d68f57d11d7f6b88c63a553f4d147644c8fc90
teniolao <Tennybetty100@gmail.com>
2022-04-28 22:24:43 +0100

    region select and css update
    css update, code refactoring, rendering all countries

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import countries from "./countriesAll.json";
 import RenderCountries from "./RenderCountries";
 
 let regions = [
+  "Filter By Region",
   "Africa",
   "Americas",
   "Antarctic",
@@ -15,11 +16,10 @@ let regions = [
 
 function App() {
   const [countryInput, setCountryInput] = useState("");
-  const [regionPicked, setRegionPicked] = useState("Africa");
+  const [regionPicked, setRegionPicked] = useState("");
 
   function selectRegion(event) {
     setRegionPicked(event.target.value);
-    //console.log(event.target.value);
   }
 
   function handleCountrySearch(event) {
@@ -27,29 +27,42 @@ function App() {
   }
 
   const filterCountries = countries.filter((country) => {
+    const countriesByRegion = country.region
+      .toLowerCase()
+      .includes(regionPicked.toLowerCase()); // this filters countries by selected region
+    const isCountryNameSearch = country.name
+      .toLowerCase()
+      .includes(countryInput.toLowerCase()); // this filters countries by name search
+    const isCountryCapitalSearch = country.capital
+      ?.toLowerCase()
+      .includes(countryInput.toLowerCase()); // this filters countries by capital search
+
     return (
-      (country.name.toLowerCase().includes(countryInput.toLowerCase()) ||
-        country.capital?.toLowerCase().includes(countryInput.toLowerCase())) &&
-      country.region.toLowerCase().includes(regionPicked.toLowerCase())
+      (isCountryNameSearch || isCountryCapitalSearch) &&
+      (regionPicked === "Filter By Region" || countriesByRegion) // returning all countries or countries by region
     );
   });
 
   return (
     <div className="App">
-      <div>
-        <input
-          placeholder="Search for a  Country..."
-          type="text"
-          onChange={handleCountrySearch}
-        ></input>
-        <select onChange={selectRegion}>
-          {regions.map((region) => {
-            return <option>{region}</option>;
-          })}
-        </select>
-      </div>
-      <div className= "container">
-        <RenderCountries countries={filterCountries} />
+      <nav className="navbar">Where in the world? </nav>
+      <div className="body--container">
+        <section className="search-input--container">
+          <input
+            placeholder="Search for a  Country..."
+            type="text"
+            onChange={handleCountrySearch}
+          ></input>
+          <select onChange={selectRegion} placeholder="Search for a Country...">
+            {regions.map((region) => {
+              return <option>{region}</option>;
+            })}
+          </select>
+        </section>
+
+        <div className="container">
+          <RenderCountries countries={filterCountries} />
+        </div>
       </div>
     </div>
   );
